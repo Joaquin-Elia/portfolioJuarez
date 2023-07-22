4bae7f6000f5bc454c87e4b3ab865599f5e78491
Joaquín Elía <joaquin.elia@hotmail.com>
2023-07-22 02:28:21 -0300

    Finished portfolio

--- js/index.js
+++ js/index.js
@@ -5,12 +5,15 @@ const projectsGallery = document.querySelector('#gallery');
 const lightbox = document.querySelector('.lightbox');
 const bigImage = document.querySelector('.expanded-img');
 const closeBtn = document.querySelector('.btn-close');
+const email = document.querySelector('#email');
+const error = document.querySelector('#form-error');
+
 
 const closeBtnHandler = () => lightbox.classList.remove('isActive')
 
 // Shuffle an array randomly
 const shuffleArray = (array) => {
-    array.forEach(index => {
+    array.forEach((element, index) => {
         // Generate a random index within the valid range
         const randomIndex = Math.floor(Math.random() * (index + 1));
 
@@ -21,15 +24,15 @@ const shuffleArray = (array) => {
 }
 
 const projects = [
-        'projectOne.png', 'projectTwo.gif', 'projectThree.gif', 'projectFour.png', 
-        'projectFive.png', 'projectSix.png', 'projectSeven.gif', 'projectEight.gif',
-        'projectNine.gif', 'projectTen.gif', 'projectEleven.gif', 'projectTwelve.gif',
-        'projectThirteen.gif', 'projectFourteen.gif', 'projectFifteen.gif', 'projectSixteen.gif'
+        'project1.gif', 'project2.gif', 'project3.gif', 'project4.gif', 
+        'project5.gif', 'project6.gif', 'project7.gif', 'project8.gif',
+        'project9.gif', 'project10.gif', 'project11.gif', 'project12.gif',
+        'project13.webp', 'project14.webp', 'project15.webp', 'project16.webp'
     ];
 
 shuffleArray(projects);
 
-let imageElements = projects.map(img => `<img class="projects-images" src="./assets/${img}" loading="lazy">`)
+let imageElements = projects.map((img, i) => `<img class="projects-images" src="./assets/${img}" loading="lazy" alt="Proyecto diseño ${i + 1}">`)
 
 projectsGallery.innerHTML += imageElements.join('');
 
@@ -39,10 +42,11 @@ const imgList = document.querySelectorAll('.projects-images');
 const imgListHandler = index => {
     lightbox.classList.add('isActive');
     bigImage.src = imgList[index].src
+    bigImage.alt = imgList[index].alt
 }
 
 imgList.forEach((img, i) => {
-    imgList[i].addEventListener('pointerup', () => 
+    imgList[i].addEventListener('click', () => 
         imgListHandler(i));
 
     let rotation = Math.random() * 12 - 6;
@@ -50,10 +54,29 @@ imgList.forEach((img, i) => {
     img.classList.add('rotated');
 });
 
-// close lightbox
-closeBtn.addEventListener('pointerup', closeBtnHandler);
+document.addEventListener('dragstart', (e) => {
+    if (e.target.tagName == 'IMG') {
+      e.preventDefault();
+    }
+});
 
-lightbox.addEventListener('pointerup', e => {
+// close lightbox
+closeBtn.addEventListener('click', closeBtnHandler);
+
+lightbox.addEventListener('click', e => {
     if (e.target !== bigImage)
         return closeBtnHandler();
 })
+
+
+document.getElementById('form').addEventListener('submit', e => {
+    e.preventDefault();
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+
+    if(!regexEmail.test(email.value)) {
+        error.innerHTML += 'El email no es válido'
+    }
+    else {
+        form.submit()
+    }
+});
